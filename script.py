63baa63f29e526e623cfd6b8a9cb10b0f686aa6f
Wing-Kit Leung <wing_kitleung@hotmail.com>
2024-10-15 12:25:19 +0100

    abstract isolated

--- script.py
+++ script.py
@@ -2,20 +2,44 @@ import requests
 from bs4 import BeautifulSoup
 # Print a greeting message
 print("Hi")
-# Send a GET request to Google
+# Send a GET request to ResearchGate
 url = "https://www.ncbi.nlm.nih.gov/pmc/articles/PMC8499625/"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
 }
-page_to_scrape = requests.get(url, headers=headers, timeout=5)
-# Check if the request was successful
-if page_to_scrape.status_code == 200:
+try:
+    # Send the request
+    page_to_scrape = requests.get(url, headers=headers, timeout=5)
+    # Check if the request was successful
+    page_to_scrape.raise_for_status()  # Raises an HTTPError for bad responses (4xx, 5xx)
     # Parse the content of the page
     soup = BeautifulSoup(page_to_scrape.content, 'html.parser')
     # Print all <div> elements (for debugging)
     all_divs = soup.findAll("div")
     for div in all_divs:
-        print(div)
-        print("1")
-else:
-    print("Failed to retrieve the page.")
+        # CHECK CLASSES
+        if 'class' in div.attrs:
+            class_given = div.attrs['class']
+            # print("class:", class_given)
+            
+        # CHECK IDs
+        if 'id' in div.attrs:
+            id_given = div.attrs['id']
+            # print("id:", id_given)
+            # CHECK for class abstract-a.c.b.q
+            if id_given == "abstract-a.c.b.q":
+                print ("yes", id_given)
+                print("div: ", div)
+                abstract = (div.getText().replace("Abstract", "").strip())
+                print("abstract: ", abstract)
+                
+        
+        
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+except requests.exceptions.ConnectionError as conn_err:
+    print(f"Connection error occurred: {conn_err}")
+except requests.exceptions.Timeout as timeout_err:
+    print(f"Timeout error occurred: {timeout_err}")
+except requests.exceptions.RequestException as req_err:
+    print(f"Error occurred: {req_err}")
